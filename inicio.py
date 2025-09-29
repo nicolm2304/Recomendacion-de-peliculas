d03204813d8a3243d27d2cf08d98072b3d2108e7
nicolm2304 <nicols-martinezc@unilibre.edu.co>
2025-09-29 12:40:14 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,3 +1,9 @@
 codigo que recomienda peliculas
 #este es un archivo de python
-print("Mensaje") 
+#keywords.csv, credits.csv,links.csv, links_small.csv, ratings_small.csv,movies_metadata.csv
+# los de ariva son archivos de conjunto de datos de peliculas para las peliculas
+import pandas as pd # Import Pandas
+
+metadata = pd.read_csv('movies_metadata.csv', low_memory=False) # Load Movies Metadata
+
+metadata.head(3) # Print the first three rows
