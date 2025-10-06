ebb2ac3aeec3771d9e5bfb97df9a78d768217dbe
nicolm2304 <nicols-martinezc@unilibre.edu.co>
2025-10-06 10:00:50 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -2,11 +2,19 @@ codigo que recomienda peliculas
 #este es un archivo de python
 #keywords.csv, credits.csv,links.csv, links_small.csv, ratings_small.csv,movies_metadata.csv
 # los de ariva son archivos de conjunto de datos de peliculas para las peliculas
-import pandas as pd # Import Pandas
+# Se importa Pandas
+import pandas as pd 
+# Se cargan las peliculas de metadata
+metadata = pd.read_csv('movies_metadata.csv', low_memory=False)
+# Imprimir las tres primeras lineas
+metadata.head(3)
 
-metadata = pd.read_csv('movies_metadata.csv', low_memory=False) # Load Movies Metadata
-
-metadata.head(3) # Print the first three rows
+# Funcion que computa y pesa el puntaje de cada pelicula
+def weighted_rating(x, m=m, C=C):
+    v = x['vote_count']
+    R = x['vote_average']
+    # Calculation based on the IMDB formula
+    return (v/(v+m) * R) + (m/(m+v) * C)
 
 # Calcular el porcentaje de votacion
 C = metadata['vote_average'].mean()
@@ -19,3 +27,5 @@ print(m)
 # se filtran todas las peliculas calificadas a una nueva lista/matriz
 q_movies = metadata.copy().loc[metadata['vote_count'] >= m]
 q_movies.shape
+
+metadata.shape
